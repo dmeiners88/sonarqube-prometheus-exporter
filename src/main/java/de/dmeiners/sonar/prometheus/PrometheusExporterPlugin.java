d51f5f979ba923b0f24ebbc9f27f6cb4d792d70d
Daniel Meiners <mail@dmeiners.de>
2018-06-16 21:01:39 +0200

    [feat] Add configuration and first implementation

--- src/main/java/de/dmeiners/sonar/prometheus/PrometheusExporterPlugin.java
+++ src/main/java/de/dmeiners/sonar/prometheus/PrometheusExporterPlugin.java
@@ -19,14 +19,28 @@
  */
 package de.dmeiners.sonar.prometheus;
 
-import de.dmeiners.sonar.prometheus.web.PrometheusWebService;
 import org.sonar.api.Plugin;
+import org.sonar.api.PropertyType;
+import org.sonar.api.config.PropertyDefinition;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class PrometheusExporterPlugin implements Plugin {
 
     @Override
     public void define(Context context) {
 
+        List<PropertyDefinition> properties = PrometheusWebService.SUPPORTED_METRICS.stream()
+            .map(metric -> PropertyDefinition.builder(PrometheusWebService.CONFIG_PREFIX + metric.getKey())
+                .name(String.format("Export \"%s\" as Prometheus metric.", metric.getName()))
+                .description(metric.getDescription())
+                .type(PropertyType.BOOLEAN)
+                .defaultValue(Boolean.TRUE.toString())
+                .build())
+            .collect(Collectors.toList());
+
+        context.addExtensions(properties);
         context.addExtension(PrometheusWebService.class);
     }
 }
---
+++ src/main/java/de/dmeiners/sonar/prometheus/PrometheusWebService.java
@@ -0,0 +1,138 @@
+package de.dmeiners.sonar.prometheus;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.common.TextFormat;
+import org.sonar.api.config.Configuration;
+import org.sonar.api.measures.CoreMetrics;
+import org.sonar.api.measures.Metric;
+import org.sonar.api.resources.Qualifiers;
+import org.sonar.api.server.ws.WebService;
+import org.sonarqube.ws.Components;
+import org.sonarqube.ws.Measures;
+import org.sonarqube.ws.client.WsClient;
+import org.sonarqube.ws.client.WsClientFactories;
+import org.sonarqube.ws.client.components.SearchRequest;
+import org.sonarqube.ws.client.measures.ComponentRequest;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Objects.nonNull;
+
+public class PrometheusWebService implements WebService {
+
+    static final Set<Metric<?>> SUPPORTED_METRICS = new HashSet<>();
+    static final String CONFIG_PREFIX = "prometheus.export.";
+
+    private final Configuration configuration;
+    private final Map<String, Gauge> gauges = new HashMap<>();
+    private final Set<Metric<?>> enabledMetrics = new HashSet<>();
+
+    static {
+
+        SUPPORTED_METRICS.add(CoreMetrics.BUGS);
+        SUPPORTED_METRICS.add(CoreMetrics.VULNERABILITIES);
+        SUPPORTED_METRICS.add(CoreMetrics.CODE_SMELLS);
+        SUPPORTED_METRICS.add(CoreMetrics.COVERAGE);
+        SUPPORTED_METRICS.add(CoreMetrics.TECHNICAL_DEBT);
+    }
+
+    public PrometheusWebService(Configuration configuration) {
+
+        this.configuration = configuration;
+    }
+
+    @Override
+    public void define(Context context) {
+
+        updateEnabledMetrics();
+        updateEnabledGauges();
+
+        NewController controller = context.createController("api/prometheus");
+        controller.setDescription("Prometheus Exporter");
+
+        controller.createAction("metrics")
+            .setHandler((request, response) -> {
+
+                updateEnabledMetrics();
+                updateEnabledGauges();
+
+                if (!this.enabledMetrics.isEmpty()) {
+
+                    WsClient wsClient = WsClientFactories.getLocal().newClient(request.localConnector());
+
+                    List<Components.Component> projects = getProjects(wsClient);
+                    projects.forEach(project -> {
+
+                        Measures.ComponentWsResponse wsResponse = getMeasures(wsClient, project);
+
+                        wsResponse.getComponent().getMeasuresList().forEach(measure -> {
+
+                            if (this.gauges.containsKey(measure.getMetric())) {
+
+                                this.gauges.get(measure.getMetric()).labels(project.getKey(), project.getName()).set(Double.valueOf(measure.getValue()));
+                            }
+                        });
+                    });
+                }
+
+                OutputStream output = response.stream()
+                    .setMediaType(TextFormat.CONTENT_TYPE_004)
+                    .setStatus(200)
+                    .output();
+
+                try (OutputStreamWriter writer = new OutputStreamWriter(output)) {
+
+                    TextFormat.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
+                }
+
+            });
+
+        controller.done();
+    }
+
+    private void updateEnabledMetrics() {
+
+        Map<Boolean, List<Metric<?>>> byEnabledState = SUPPORTED_METRICS.stream()
+            .collect(Collectors.groupingBy(metric -> this.configuration.getBoolean(CONFIG_PREFIX + metric.getKey()).orElse(false)));
+
+        this.enabledMetrics.clear();
+
+        if (nonNull(byEnabledState.get(true))) {
+            this.enabledMetrics.addAll(byEnabledState.get(true));
+        }
+    }
+
+    private void updateEnabledGauges() {
+
+        CollectorRegistry.defaultRegistry.clear();
+
+        this.enabledMetrics.forEach(metric -> gauges.put(metric.getKey(), Gauge.build()
+            .name("sonar_" + metric.getKey())
+            .help(metric.getDescription())
+            .labelNames("key", "name")
+            .register()));
+    }
+
+    private Measures.ComponentWsResponse getMeasures(WsClient wsClient, Components.Component project) {
+
+        List<String> metricKeys = this.enabledMetrics.stream()
+            .map(Metric::getKey)
+            .collect(Collectors.toList());
+
+        return wsClient.measures().component(new ComponentRequest()
+            .setComponent(project.getKey())
+            .setMetricKeys(metricKeys));
+    }
+
+    private List<Components.Component> getProjects(WsClient wsClient) {
+
+        return wsClient.components().search(new SearchRequest()
+            .setQualifiers(Collections.singletonList(Qualifiers.PROJECT))
+            .setPs("500"))
+            .getComponentsList();
+    }
+}
