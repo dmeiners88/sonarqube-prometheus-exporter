35b2c274925af6646144d88576b906ac7d6d1870
Bheem <bheemvennapureddy@curo.com>
2022-07-11 22:02:04 -0500

    update metrcis and latest dependencies

--- src/main/java/de/dmeiners/sonar/prometheus/PrometheusWebService.java
+++ src/main/java/de/dmeiners/sonar/prometheus/PrometheusWebService.java
@@ -39,6 +39,20 @@ public class PrometheusWebService implements WebService {
         SUPPORTED_METRICS.add(CoreMetrics.CODE_SMELLS);
         SUPPORTED_METRICS.add(CoreMetrics.COVERAGE);
         SUPPORTED_METRICS.add(CoreMetrics.TECHNICAL_DEBT);
+        SUPPORTED_METRICS.add(CoreMetrics.COMPLEXITY);
+        SUPPORTED_METRICS.add(CoreMetrics.BLOCKER_VIOLATIONS);
+        SUPPORTED_METRICS.add(CoreMetrics.CRITICAL_VIOLATIONS);
+        SUPPORTED_METRICS.add(CoreMetrics.MAJOR_VIOLATIONS);
+        SUPPORTED_METRICS.add(CoreMetrics.MINOR_VIOLATIONS);
+        SUPPORTED_METRICS.add(CoreMetrics.INFO_VIOLATIONS);
+        SUPPORTED_METRICS.add(CoreMetrics.DUPLICATED_LINES_DENSITY);
+        SUPPORTED_METRICS.add(CoreMetrics.DUPLICATED_BLOCKS);
+        SUPPORTED_METRICS.add(CoreMetrics.DUPLICATED_LINES);
+        SUPPORTED_METRICS.add(CoreMetrics.DUPLICATED_FILES);
+        SUPPORTED_METRICS.add(CoreMetrics.OPEN_ISSUES);
+        SUPPORTED_METRICS.add(CoreMetrics.NCLOC);
+        SUPPORTED_METRICS.add(CoreMetrics.COGNITIVE_COMPLEXITY);
+
     }
 
     public PrometheusWebService(Configuration configuration) {
@@ -56,41 +70,40 @@ public class PrometheusWebService implements WebService {
         controller.setDescription("Prometheus Exporter");
 
         controller.createAction("metrics")
-            .setHandler((request, response) -> {
+                .setHandler((request, response) -> {
 
-                updateEnabledMetrics();
-                updateEnabledGauges();
+                    updateEnabledMetrics();
+                    updateEnabledGauges();
 
-                if (!this.enabledMetrics.isEmpty()) {
+                    if (!this.enabledMetrics.isEmpty()) {
 
-                    WsClient wsClient = WsClientFactories.getLocal().newClient(request.localConnector());
+                        WsClient wsClient = WsClientFactories.getLocal().newClient(request.localConnector());
 
-                    List<Components.Component> projects = getProjects(wsClient);
-                    projects.forEach(project -> {
+                        List<Components.Component> projects = getProjects(wsClient);
+                        projects.forEach(project -> {
 
-                        Measures.ComponentWsResponse wsResponse = getMeasures(wsClient, project);
+                            Measures.ComponentWsResponse wsResponse = getMeasures(wsClient, project);
 
-                        wsResponse.getComponent().getMeasuresList().forEach(measure -> {
+                            wsResponse.getComponent().getMeasuresList().forEach(measure -> {
 
-                            if (this.gauges.containsKey(measure.getMetric())) {
-
-                                this.gauges.get(measure.getMetric()).labels(project.getKey(), project.getName()).set(Double.valueOf(measure.getValue()));
-                            }
+                                if (this.gauges.containsKey(measure.getMetric())) {
+                                    this.gauges.get(measure.getMetric()).labels(project.getKey(), project.getName()).set(Double.valueOf(measure.getValue()));
+                                }
+                            });
                         });
-                    });
-                }
+                    }
 
-                OutputStream output = response.stream()
-                    .setMediaType(TextFormat.CONTENT_TYPE_004)
-                    .setStatus(200)
-                    .output();
+                    OutputStream output = response.stream()
+                            .setMediaType(TextFormat.CONTENT_TYPE_004)
+                            .setStatus(200)
+                            .output();
 
-                try (OutputStreamWriter writer = new OutputStreamWriter(output)) {
+                    try (OutputStreamWriter writer = new OutputStreamWriter(output)) {
 
-                    TextFormat.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
-                }
+                        TextFormat.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
+                    }
 
-            });
+                });
 
         controller.done();
     }
@@ -98,7 +111,7 @@ public class PrometheusWebService implements WebService {
     private void updateEnabledMetrics() {
 
         Map<Boolean, List<Metric<?>>> byEnabledState = SUPPORTED_METRICS.stream()
-            .collect(Collectors.groupingBy(metric -> this.configuration.getBoolean(CONFIG_PREFIX + metric.getKey()).orElse(false)));
+                .collect(Collectors.groupingBy(metric -> this.configuration.getBoolean(CONFIG_PREFIX + metric.getKey()).orElse(false)));
 
         this.enabledMetrics.clear();
 
@@ -112,28 +125,28 @@ public class PrometheusWebService implements WebService {
         CollectorRegistry.defaultRegistry.clear();
 
         this.enabledMetrics.forEach(metric -> gauges.put(metric.getKey(), Gauge.build()
-            .name(METRIC_PREFIX + metric.getKey())
-            .help(metric.getDescription())
-            .labelNames("key", "name")
-            .register()));
+                .name(METRIC_PREFIX + metric.getKey())
+                .help(metric.getDescription())
+                .labelNames("key", "name")
+                .register()));
     }
 
     private Measures.ComponentWsResponse getMeasures(WsClient wsClient, Components.Component project) {
 
         List<String> metricKeys = this.enabledMetrics.stream()
-            .map(Metric::getKey)
-            .collect(Collectors.toList());
+                .map(Metric::getKey)
+                .collect(Collectors.toList());
 
         return wsClient.measures().component(new ComponentRequest()
-            .setComponent(project.getKey())
-            .setMetricKeys(metricKeys));
+                .setComponent(project.getKey())
+                .setMetricKeys(metricKeys));
     }
 
     private List<Components.Component> getProjects(WsClient wsClient) {
 
         return wsClient.components().search(new SearchRequest()
-            .setQualifiers(Collections.singletonList(Qualifiers.PROJECT))
-            .setPs("500"))
-            .getComponentsList();
+                .setQualifiers(Collections.singletonList(Qualifiers.PROJECT))
+                .setPs("500"))
+                .getComponentsList();
     }
 }
